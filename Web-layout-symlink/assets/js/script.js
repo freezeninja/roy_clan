6cbf1eb67d0897ff1f8acc49c7669367d25abcdb
Arijit <arijitmaiti085@gmail.com>
2022-04-04 08:45:15 +0530

    4th aprill

--- Web-layout-symlink/assets/js/script.js
+++ Web-layout-symlink/assets/js/script.js
@@ -1,14 +1,29 @@
-$(document).ready(function(){
-	"use strict";
+// $(document).ready(function(){
+// 	"use strict";
 
 
-});
+// });
 
-$(window).on('load', function(){
-	"use strict";
-	setTimeout(function(){
-		$('.preloader').addClass('inactive');
-	}, 500);
+// $(window).on('load', function(){
+// 	"use strict";
+// 	setTimeout(function(){
+// 		$('.preloader').addClass('inactive');
+// 	}, 500);
 	
-});
+// });
+
+
+
+const luxary_studio = document.getElementById('luxary_studio');
+const per_studio_img = document.querySelectorAll('.per_studio_img');
+
+per_studio_img.forEach((e,i)=>{
+	e.addEventListener('click', ()=>{
+		console.log(e);
+		luxary_studio.style.backgroundImage = `url('${e.src}')`;
+		luxary_studio.style.backgroundRepeat = 'no-repeat';
+		luxary_studio.style.backgroundSize ='cover';
+		luxary_studio.style.backgroundPosition = 'center'
+	});
+})
 
